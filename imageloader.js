db0ce676fddab9e2084b7145a5032e1ef74927a5
navyxie <navyxie2010@gmail.com>
2016-04-27 03:28:50 +0800

    first version

--- imageloader.js
+++ imageloader.js
@@ -9,36 +9,133 @@
     root.ImageLoader = factory();
   }
 }(this, function() {
-    function noop(){}
-    //图片预先加载
-    function preLoad(imgArr,updatecb,endcb){
-        updatecb = updatecb || noop;
-        endcb = endcb || noop;
-        var count = 0;
-        for(var i = 0 , len = imgArr.length ; i < len; i++){
-            loadImg(imgArr[i],function(){  
-                ++count
-                updatecb(count,len);        
-                if(count === len){
-                    endcb();
-                }
-            })
+    var doc = document;
+    var isReady = false;
+    var readyCbList = [];
+    function _domContentLoadedFn(){
+        isReady = true;
+        doc.removeEventListener('DOMContentLoaded',_domContentLoadedFn,false);        
+        var fireFn;
+        while(fireFn = readyCbList.shift()){
+            fireFn();
         }
     }
+    _ready = function(cb){
+        if(isReady || doc.readyState === "complete"){
+            return cb();
+        }
+        readyCbList.push(cb);
+        doc.addEventListener('DOMContentLoaded',_domContentLoadedFn,false);
+    }
+    function noop(){}
+     
+    function isObject(obj){
+        return Object.prototype.toString.call(obj) === '[object Object]';
+    }
+    function isFunction(fn){
+        return Object.prototype.toString.call(fn) === '[object Function]';
+    }
+    function _createElement(tagName,attrs){
+        if(isObject(tagName)){
+            attrs = tagName;
+            tagName = 'DIV';
+        }
+        var dom = doc.createElement(tagName);
+        if(isObject(attrs)){
+            for(var key in attrs){
+                if(attrs.hasOwnProperty(key)){
+                    if(key === 'className'){
+                        dom.setAttribute('class',attrs[key])
+                    }else{
+                        dom.setAttribute(key,attrs[key])
+                    }                  
+                }
+            }
+        }
+        return dom;
+    }
+    function _createTextNode(str){
+        return doc.createTextNode(str);
+    }
+    function _appendChild(parentDom,subDom){
+        parentDom.appendChild(subDom);
+        return parentDom;
+    } 
+    function _initDom(){
+        var body =  doc.getElementsByTagName('body')[0];
+        var df = doc.createDocumentFragment();
+        var container = _createElement({id:'imageLoaderMaskContainer',className:'imageLoaderMaskContainer'});
+        var content = _createElement({className:'loadingContent'});
+        var imageLoaderIn = _createElement({id:'imageLoaderIn',className:'imageLoaderIn'});
+        var textContent = _createElement({className:'textContent'});
+        var span = _createElement('SPAN',{id:'loadPercent'});
+        var text = _createTextNode('%');
+        _appendChild(textContent,span);
+        _appendChild(textContent,text);
+        _appendChild(content,imageLoaderIn);
+        _appendChild(content,textContent);
+        _appendChild(container,content);
+        _appendChild(df,container);
+        _appendChild(body,df);
+    }
+    function _removeDom(){
+        doc.getElementsByTagName('body')[0].removeChild(doc.getElementById('imageLoaderMaskContainer'));
+    }
+    //图片预先加载
+    function preLoad(imgArr,config){
+        var endcb = noop;
+        var cleanfn = _removeDom
+        var initfn = _initDom;
+        var updatecb = _updateLoadingCb;
+        if(isFunction(config)){
+            endcb = function(){
+                _removeDom();
+                config();
+            }
+        }else if(isObject(config)){
+            if(isFunction(config.complete)){
+                endcb = config.complete;
+            }
+            if(isFunction(config.update)){
+                if(config.silent){
+                    updatecb = function(count,len,url){
+                        _updateLoadingCb(count,len,url);
+                        config.update(count,len,url);
+                    }
+                }else{
+                    initfn = cleanfn = noop;
+                    updatecb = config.update;
+                }
+            }
+        }
+        _ready(function(){
+            initfn();
+            var count = 0;
+            for(var i = 0 , len = imgArr.length ; i < len; i++){
+                _loadImg(imgArr[i],function(url){  
+                    ++count
+                    updatecb(count,len,url);        
+                    if(count === len){ 
+                        cleanfn();                       
+                        endcb();
+                    }
+                })
+            }
+        });
+    }
     //加载单张图片
-    function loadImg(src,cb){
+    function _loadImg(src,cb){
         var imgObj = new Image();
-        imgObj.onerror = imgObj.onload = cb;
+        imgObj.onerror = imgObj.onload = function(){cb(src)};
         imgObj.src = src;
         imgObj = null;
     }
     //加载单张图片的回调
-    function updateLoadingCb(count,len){
-        document.getElementById('loadPercent').innerHTML = ((count/len)*100).toFixed(2);
-        document.getElementById('inlineBg').style.width = 240*count/len+'px';
+    function _updateLoadingCb(count,len){
+        console.log(1);
+        var loadTextWidth =  doc.getElementById('imageLoaderMaskContainer').children[0].offsetWidth;
+        doc.getElementById('loadPercent').innerHTML = ((count/len)*100).toFixed(2);
+        doc.getElementById('imageLoaderIn').style.width = loadTextWidth*count/len+'px';
     }
-    ImageLoader.preLoad = preLoad;
-    ImageLoader.loadImg = loadImg;
-    ImageLoader.version = '0.0.1';
-    return ImageLoader;
+    return preLoad;
 }));
